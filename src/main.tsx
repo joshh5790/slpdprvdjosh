faddd50c65debe8224e25ad0f61fb446ddbee44a
Josh Ho <josh@open.store>
2025-04-05 14:08:00 -0400

    move appContext to src

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { AppProvider } from './appContext.tsx'
+import { AppProvider } from './appContext'
 import './index.css'
 import App from './App.tsx'
 
